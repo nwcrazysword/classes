e07170d5432ed40170510e701ce8e7715f1d4684
nwcrazysword <nwcrazysword@gmail.com>
2017-04-08 23:12:20 +0800

    add the 20170406 task code

---
+++ 20170406/task/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-08 12:58:54
+# @Author  : nwcrazysword (nwcrazysword@gmail.com)
+# @Link    : https://github.com/nwcrazysword
+
+import re
+
+# read content from text file
+def getcontent(fpath):
+    with open(fpath, 'r') as f:
+        return f.read(8089)
+
+# clean up the content
+def cleancontent(content):
+    seplist = '.\-,():'
+    content = re.sub(r'[%s]+' % seplist, ' ', content)
+    return content
+
+
+# get the list of the word
+def genwords(content):
+    def isword(word):
+        word = word.strip()
+        if word:
+            return True
+        else:
+            return False
+    return filter(isword, content.split())
+
+# count and sorted
+def countwords(words):
+    rdict = {}
+    for word in words:
+        num = rdict.get(word, 0)
+        rdict[word] = num + 1
+    rlist = sorted(rdict.items(), key=lambda x: x[1], reverse=True)
+    return rlist
+
+# save to file
+def saveresult(rpath, rlist):
+    lines = ''
+    with open(rpath, 'w') as f:
+        for word, num in rlist:
+            line = '%-15s -> %2d \n' %(word,num)
+            lines += line
+        f.write(lines)
+
+def main():
+    fpath = r'doc.txt'
+    rpath = r'result.txt'
+
+    content = getcontent(fpath)
+    content = cleancontent(content)
+    words = genwords(content)
+    rlist = countwords(words)
+    saveresult(rpath, rlist)
+
+if __name__ == '__main__':
+    main()
